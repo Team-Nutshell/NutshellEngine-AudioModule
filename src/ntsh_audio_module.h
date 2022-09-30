0eb73f16d32f98c01443be431c5fff9dfe63e40b
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-30 19:47:40 +0200

    initial audio module

---
+++ src/ntsh_audio_module.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../external/Common/module_interfaces/ntsh_audio_module_interface.h"
+
+class NutshellAudioModule : public NutshellAudioModuleInterface {
+public:
+    NutshellAudioModule() : NutshellAudioModuleInterface("Nutshell Audio Test Module") {}
+
+    void init();
+    void update(double dt);
+    void destroy();
+};
