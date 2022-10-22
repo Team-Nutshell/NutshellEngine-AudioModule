bc74fb7f6209168eeb535c646213c3f9ed5e3774
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-22 21:06:51 +0200

    add new functions to default module

--- src/ntsh_audio_module.cpp
+++ src/ntsh_audio_module.cpp
@@ -17,6 +17,52 @@ void NutshellAudioModule::destroy() {
 	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
+NtshAudioId NutshellAudioModule::play(const NtshAudio& audio) {
+	NTSH_UNUSED(audio);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+	return 0;
+}
+
+void NutshellAudioModule::pause(NtshAudioId audioId) {
+	NTSH_UNUSED(audioId);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+}
+
+void NutshellAudioModule::stop(NtshAudioId audioId) {
+	NTSH_UNUSED(audioId);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+}
+
+bool NutshellAudioModule::isPlaying(NtshAudioId audioId) {
+	NTSH_UNUSED(audioId);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+	return false;
+}
+
+void NutshellAudioModule::setGain(NtshAudioId audioId, float newGain) {
+	NTSH_UNUSED(audioId);
+	NTSH_UNUSED(newGain);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+}
+
+float NutshellAudioModule::getGain(NtshAudioId audioId) {
+	NTSH_UNUSED(audioId);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+	return 0.0f;
+}
+
+void NutshellAudioModule::setPitch(NtshAudioId audioId, float newPitch) {
+	NTSH_UNUSED(audioId);
+	NTSH_UNUSED(newPitch);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+}
+
+float NutshellAudioModule::getPitch(NtshAudioId audioId) {
+	NTSH_UNUSED(audioId);
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
+	return 0.0f;
+}
+
 extern "C" NTSH_MODULE_API NutshellAudioModuleInterface* createModule() {
 	return new NutshellAudioModule;
 }
--- src/ntsh_audio_module.h
+++ src/ntsh_audio_module.h
@@ -8,4 +8,24 @@ public:
 	void init();
 	void update(double dt);
 	void destroy();
+
+	// Plays an audio and returns an identifier
+	NtshAudioId play(const NtshAudio& audio);
+	// Pauses the audio with identifier audioId
+	void pause(NtshAudioId audioId);
+	// Stops the audio with identifier audioId
+	void stop(NtshAudioId audioId);
+
+	// Returns true if the audio with identifier audioId is currently playing, else, returns false
+	bool isPlaying(NtshAudioId audioId);
+
+	// Sets the gain of the audio with identifier audioId
+	void setGain(NtshAudioId audioId, float newGain);
+	// Gets the gain of the audio with identifier audioId
+	float getGain(NtshAudioId audioId);
+
+	// Sets the pitch of the audio with identifier audioId
+	void setPitch(NtshAudioId audioId, float newPitch);
+	// Gets the pitch of the audio with identifier audioId
+	float getPitch(NtshAudioId audioId);
 };
