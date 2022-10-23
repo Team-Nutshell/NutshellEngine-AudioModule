9a5922e3fd7f8b2f9f93898d8901fdf54d59854c
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-23 14:57:40 +0200

    fix include paths

--- src/ntsh_audio_module.cpp
+++ src/ntsh_audio_module.cpp
@@ -1,8 +1,8 @@
 #include "ntsh_audio_module.h"
-#include "../external/Module/ntsh_module_defines.h"
-#include "../external/Module/ntsh_dynamic_library.h"
-#include "../external/Common/ntsh_engine_defines.h"
-#include "../external/Common/ntsh_engine_enums.h"
+#include "../external/Module/utils/ntsh_module_defines.h"
+#include "../external/Module/utils/ntsh_dynamic_library.h"
+#include "../external/Common/utils/ntsh_engine_defines.h"
+#include "../external/Common/utils/ntsh_engine_enums.h"
 
 void NutshellAudioModule::init() {
 	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
