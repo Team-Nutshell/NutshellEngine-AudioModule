3730c886e90f511c7ac40879f32a8e2ed376d635
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-14 01:23:19 +0100

    fix missing changes

--- src/ntsh_audio_module.h
+++ src/ntsh_audio_module.h
@@ -9,26 +9,26 @@ public:
 	void update(double dt);
 	void destroy();
 
-	// Loads the sound described in the audio parameter in the internal format and returns a unique identifier
-	Ntsh::SoundId load(const Ntsh::Sound audio);
+	// Loads the sound described in the sound parameter in the internal format and returns a unique identifier
+	Ntsh::SoundId load(const Ntsh::Sound sound);
 
 	// Plays the sound with identifier soundId
-	void play(Ntsh::SoundId audioId);
+	void play(Ntsh::SoundId soundId);
 	// Pauses the sound with identifier soundId
-	void pause(Ntsh::SoundId audioId);
+	void pause(Ntsh::SoundId soundId);
 	// Stops the sound with identifier soundId
-	void stop(Ntsh::SoundId audioId);
+	void stop(Ntsh::SoundId soundId);
 
 	// Returns true if the sound with identifier soundId is currently playing, else, returns false
-	bool isPlaying(Ntsh::SoundId audioId);
+	bool isPlaying(Ntsh::SoundId soundId);
 
 	// Sets the gain of the sound with identifier soundId
-	void setGain(Ntsh::SoundId audioId, float newGain);
+	void setGain(Ntsh::SoundId soundId, float newGain);
 	// Gets the gain of the sound with identifier soundId
-	float getGain(Ntsh::SoundId audioId);
+	float getGain(Ntsh::SoundId soundId);
 
 	// Sets the pitch of the sound with identifier soundId
-	void setPitch(Ntsh::SoundId audioId, float newPitch);
+	void setPitch(Ntsh::SoundId soundId, float newPitch);
 	// Gets the pitch of the sound with identifier soundId
-	float getPitch(Ntsh::SoundId audioId);
+	float getPitch(Ntsh::SoundId soundId);
 };
